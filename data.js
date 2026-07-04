193ed6eb8fbf22feba0eb124ee16b844cf5dd2e4
my-web-lb <sumica.jp@outlook.com>
2026-07-04 16:45:21 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,92 @@
-
+const funds = [
+  {
+    name: "インベスコ 世界厳選株式",
+    type: "投資信託",
+    yield: "高め",
+    frequency: "毎月分配",
+    fee: "高め",
+    nisa: "対象外の場合あり",
+    tags: ["毎月分配", "世界株", "投信"]
+  },
+  {
+    name: "Tracers 日経平均高配当株50",
+    type: "投資信託",
+    yield: "中〜高",
+    frequency: "年4回",
+    fee: "低め",
+    nisa: "NISA",
+    tags: ["日本株", "高配当", "NISA", "投信"]
+  },
+  {
+    name: "楽天SCHD",
+    type: "投資信託",
+    yield: "中",
+    frequency: "年4回",
+    fee: "中",
+    nisa: "NISA",
+    tags: ["米国株", "高配当", "NISA", "投信"]
+  },
+  {
+    name: "SBI SCHD",
+    type: "投資信託",
+    yield: "中",
+    frequency: "年4回",
+    fee: "中",
+    nisa: "NISA",
+    tags: ["米国株", "高配当", "NISA", "投信"]
+  },
+  {
+    name: "1489 日経高配当株50 ETF",
+    type: "ETF",
+    yield: "中〜高",
+    frequency: "年4回",
+    fee: "低め",
+    nisa: "NISA",
+    tags: ["日本株", "ETF", "高配当", "NISA"]
+  },
+  {
+    name: "VYM",
+    type: "海外ETF",
+    yield: "中",
+    frequency: "年4回",
+    fee: "低い",
+    nisa: "NISA",
+    tags: ["米国株", "ETF", "高配当", "NISA"]
+  },
+  {
+    name: "HDV",
+    type: "海外ETF",
+    yield: "中",
+    frequency: "年4回",
+    fee: "低い",
+    nisa: "NISA",
+    tags: ["米国株", "ETF", "高配当", "NISA"]
+  },
+  {
+    name: "SPYD",
+    type: "海外ETF",
+    yield: "中〜高",
+    frequency: "年4回",
+    fee: "低い",
+    nisa: "NISA",
+    tags: ["米国株", "ETF", "高配当", "NISA"]
+  },
+  {
+    name: "QYLD",
+    type: "海外ETF",
+    yield: "高め",
+    frequency: "毎月分配",
+    fee: "中",
+    nisa: "対象外の場合あり",
+    tags: ["毎月分配", "米国株", "ETF", "高分配"]
+  },
+  {
+    name: "2865 グローバルX NASDAQ100・カバード・コール ETF",
+    type: "国内ETF",
+    yield: "高め",
+    frequency: "毎月分配",
+    fee: "中",
+    nisa: "NISA",
+    tags: ["毎月分配", "国内ETF", "高分配", "NISA"]
+  }
+];
