0c1585cc755a34617bbccf87a1795989055c9329
my-web-lb <sumica.jp@outlook.com>
2026-07-04 18:21:57 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,21 +1,39 @@
 const funds = [
   {
-    name: "インベスコ 世界厳選株式",
+    name: "インベスコ 世界厳選株式オープン",
     type: "投資信託",
     yield: "高め",
     frequency: "毎月分配",
     fee: "高め",
     nisa: "対象外の場合あり",
-    tags: ["毎月分配", "世界株", "投信", "高配当"]
+    tags: ["世界のベスト", "毎月分配", "世界株", "投信", "高配当"]
   },
   {
-    name: "Tracers 日経平均高配当株50",
+    name: "WCM 世界成長株厳選ファンド",
     type: "投資信託",
-    yield: "中〜高",
-    frequency: "年4回",
-    fee: "低め",
+    yield: "高め",
+    frequency: "毎月分配",
+    fee: "高め",
+    nisa: "対象外の場合あり",
+    tags: ["WCM", "世界株", "成長株", "投信", "高配当"]
+  },
+  {
+    name: "楽天・JEPQ",
+    type: "投資信託",
+    yield: "高め",
+    frequency: "毎月分配",
+    fee: "中〜高",
     nisa: "NISA",
-    tags: ["日本株", "高配当", "NISA", "投信"]
+    tags: ["楽天JEPQ", "毎月分配", "NASDAQ100", "投信", "高配当"]
+  },
+  {
+    name: "563A GXNAS100Dカバコ",
+    type: "ETF",
+    yield: "高め",
+    frequency: "毎月分配",
+    fee: "中",
+    nisa: "NISA",
+    tags: ["563A", "カバコ", "毎月分配", "NASDAQ100", "ETF", "高配当"]
   },
   {
     name: "楽天SCHD",
@@ -24,7 +42,7 @@ const funds = [
     frequency: "年4回",
     fee: "中",
     nisa: "NISA",
-    tags: ["米国株", "高配当", "NISA", "投信"]
+    tags: ["楽天SCHD", "米国高配当", "米国株", "NISA", "投信"]
   },
   {
     name: "SBI SCHD",
@@ -33,7 +51,7 @@ const funds = [
     frequency: "年4回",
     fee: "中",
     nisa: "NISA",
-    tags: ["米国株", "高配当", "NISA", "投信"]
+    tags: ["SBI SCHD", "米国高配当", "米国株", "NISA", "投信"]
   },
   {
     name: "1489 日経高配当株50 ETF",
@@ -63,15 +81,6 @@ const funds = [
     tags: ["米国株", "ETF", "高配当", "NISA"]
   },
   {
-    name: "SPYD",
-    type: "海外ETF",
-    yield: "中〜高",
-    frequency: "年4回",
-    fee: "低い",
-    nisa: "NISA",
-    tags: ["米国株", "ETF", "高配当", "NISA"]
-  },
-  {
     name: "QYLD",
     type: "海外ETF",
     yield: "高め",
@@ -79,14 +88,5 @@ const funds = [
     fee: "中",
     nisa: "対象外の場合あり",
     tags: ["毎月分配", "米国株", "ETF", "高配当"]
-  },
-  {
-    name: "2865 グローバルX NASDAQ100・カバード・コール ETF",
-    type: "国内ETF",
-    yield: "高め",
-    frequency: "毎月分配",
-    fee: "中",
-    nisa: "NISA",
-    tags: ["毎月分配", "国内ETF", "高配当", "NISA"]
   }
 ];
