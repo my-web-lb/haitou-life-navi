d13f2ceed16eceeda236c3e1eb303269eead5424
my-web-lb <sumica.jp@outlook.com>
2026-07-05 09:59:46 +0900

    Update data.js

--- data.js
+++ data.js
@@ -3,109 +3,139 @@ const funds = [
     name: "インベスコ 世界厳選株式オープン",
     type: "投資信託",
     yield: "高め",
+    yieldDisplay: "高水準",
     frequency: "毎月分配",
     fee: "高め",
     nisa: "対象外",
+    badge: "人気No.1",
     url: "funds/invesco.html",
-    tags: ["世界のベスト", "毎月分配", "世界株", "投信", "高配当"]
+    tags: ["世界のベスト", "毎月分配", "世界株", "投信", "高配当"],
+    point: "毎月分配を重視する人に人気"
   },
 
   {
     name: "WCM 世界成長株厳選ファンド",
     type: "投資信託",
     yield: "高め",
+    yieldDisplay: "高水準",
     frequency: "分配型",
     fee: "高め",
     nisa: "対象外",
+    badge: "成長株",
     url: "funds/wcm.html",
-    tags: ["WCM", "世界株", "成長株", "投信", "高配当"]
+    tags: ["WCM", "世界株", "成長株", "投信", "高配当"],
+    point: "世界の成長企業に投資"
   },
 
   {
     name: "【1489】日経高配当株50",
     type: "ETF",
     yield: "中〜高",
+    yieldDisplay: "安定型",
     frequency: "年4回",
     fee: "低め",
     nisa: "NISA成長投資枠",
+    badge: "NISA対応",
     url: "funds/1489.html",
-    tags: ["1489", "日本株", "ETF", "高配当", "NISA"]
+    tags: ["1489", "日本株", "ETF", "高配当", "NISA"],
+    point: "日本高配当株ETFの定番候補"
   },
 
   {
     name: "【563A】GXNAS100Dカバコ",
     type: "ETF",
     yield: "高め",
+    yieldDisplay: "高水準",
     frequency: "毎月分配",
     fee: "中",
     nisa: "対象外",
+    badge: "毎月分配",
     url: "funds/563a.html",
-    tags: ["563A", "GXNAS100Dカバコ", "毎月分配", "NASDAQ100", "ETF", "高配当"]
+    tags: ["563A", "GXNAS100Dカバコ", "毎月分配", "NASDAQ100", "ETF", "高配当"],
+    point: "NASDAQ100系のカバードコールETF"
   },
 
   {
     name: "楽天・JEPQ",
     type: "投資信託",
     yield: "高め",
+    yieldDisplay: "高水準",
     frequency: "毎月分配",
     fee: "中〜高",
     nisa: "対象外",
+    badge: "楽天系",
     url: "funds/rakuten-jepq.html",
-    tags: ["楽天JEPQ", "毎月分配", "NASDAQ100", "投信", "高配当"]
+    tags: ["楽天JEPQ", "毎月分配", "NASDAQ100", "投信", "高配当"],
+    point: "毎月インカムを狙う楽天系ファンド"
   },
 
   {
     name: "楽天SCHD",
     type: "投資信託",
     yield: "中",
+    yieldDisplay: "中水準",
     frequency: "年4回",
     fee: "中",
     nisa: "NISA成長投資枠",
+    badge: "米国高配当",
     url: "funds/rakuten-schd.html",
-    tags: ["楽天SCHD", "米国高配当", "米国株", "NISA", "投信"]
+    tags: ["楽天SCHD", "米国高配当", "米国株", "NISA", "投信"],
+    point: "米国高配当株へ分散投資"
   },
 
   {
     name: "SBI SCHD",
     type: "投資信託",
     yield: "中",
+    yieldDisplay: "中水準",
     frequency: "年4回",
     fee: "中",
     nisa: "NISA成長投資枠",
+    badge: "SBI系",
     url: "funds/sbi-schd.html",
-    tags: ["SBI SCHD", "米国高配当", "米国株", "NISA", "投信"]
+    tags: ["SBI SCHD", "米国高配当", "米国株", "NISA", "投信"],
+    point: "SBI証券ユーザー向け候補"
   },
 
   {
     name: "VYM",
     type: "海外ETF",
     yield: "中",
+    yieldDisplay: "中水準",
     frequency: "年4回",
     fee: "低い",
     nisa: "NISA成長投資枠",
+    badge: "海外ETF",
     url: "funds/vym.html",
-    tags: ["米国株", "ETF", "高配当", "NISA"]
+    tags: ["米国株", "ETF", "高配当", "NISA"],
+    point: "米国高配当ETFの代表格"
   },
 
   {
     name: "HDV",
     type: "海外ETF",
     yield: "中",
+    yieldDisplay: "中水準",
     frequency: "年4回",
     fee: "低い",
     nisa: "NISA成長投資枠",
+    badge: "海外ETF",
     url: "funds/hdv.html",
-    tags: ["米国株", "ETF", "高配当", "NISA"]
+    tags: ["米国株", "ETF", "高配当", "NISA"],
+    point: "財務健全性を重視した米国高配当ETF"
   },
 
   {
     name: "QYLD",
     type: "海外ETF",
     yield: "高め",
+    yieldDisplay: "高水準",
     frequency: "毎月分配",
     fee: "中",
     nisa: "対象外",
+    badge: "毎月分配",
     url: "funds/qyld.html",
-    tags: ["毎月分配", "米国株", "ETF", "高配当"]
+    tags: ["毎月分配", "米国株", "ETF", "高配当"],
+    point: "毎月分配を狙う海外ETF"
   }
 ];
